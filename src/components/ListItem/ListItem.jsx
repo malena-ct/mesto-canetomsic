269a2a1841f7a52560030f76397dfbd747b9c43c
Malena Cane Tomsic <malenact@MacBook-Pro-de-Malena.local>
2021-11-27 14:30:34 -0300

    rutas

--- src/components/ListItem/ListItem.jsx
+++ src/components/ListItem/ListItem.jsx
@@ -4,7 +4,7 @@ import { NavLink } from 'react-router-dom'
 export default function ListItem({item}) {
     return (
         <li className='nav-item'>
-            <NavLink className="nav-link" to={`/category/${item}`}>
+            <NavLink activeclassname="active" className="nav-link" to={`/category/${item}`}>
                 <p> {item}</p>
             </NavLink>
         </li>
